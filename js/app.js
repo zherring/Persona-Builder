fbf3d885520e22cd3825f683e0231d7192c26504
zherring <z.herring@gmail.com>
2017-01-02 08:57:03 -0700

    data binding, adding and deleting attributes
    ah, except you gotta create a new attr in each person, prob remove them
    too as you delete. 🤔

--- js/app.js
+++ js/app.js
@@ -1,38 +1,41 @@
 
-let personas = [];
-let personaTemplate = {
-    name: "John Doe",
-    attributes: [
-        {
-            name: "JohnDoe",
-            range: 50,
-            notes: "These are notes"
-        }
-    ]
-};
-
 new Vue({
     el: '#personaMain',
     data: {
         header: "Persona Creator",
         people: [
-            { name: "Jeff",
-              range: 97,
-              notes: "Nada"
-            }
-        ]
+            {
+                name: "Jeff",
+                attrData: [
+                { name: "Attr Name", range: 57, notes: "Nada"},
+                { name: "Attr Name2", range: 67, notes: "Nada"},
+                { name: "Attr Name3", range: 98, notes: "Nada"}
+                ]
+              },
+        ],
+        attributes: ["Attribute 1", "Attribute 2", "Attribute 3"]
     },
     methods: {
         createPersona: function() {
             this.people.push({
             name: "John Doe",
-              range: 50,
-              notes: "Nada"
+            attrData: [
+            { name: "Attr Name", range: 57, notes: "Nada"},
+            { name: "Attr Name2", range: 67, notes: "Nada"},
+            { name: "Attr Name3", range: 98, notes: "Nada"}
+            ]
           });
-      },
+        },
         deletePersona: function(index) {
                 this.people.splice(index, 1);
-            }
+        },
+        addAttr: function() {
+            this.attributes.push("New Attribute");
+
+        },
+        removeAttr: function(index) {
+                this.attributes.splice(index, 1);
+        },
     }
 });
 
