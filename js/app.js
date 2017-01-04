dcd38a91219278adf1df17cd745c67fd95516eec
zherring <z.herring@gmail.com>
2017-01-04 14:26:45 -0700

    removing ref code

--- js/app.js
+++ js/app.js
@@ -60,31 +60,3 @@ new Vue({
         this.addAttr("Custom Attribute 3");
       }
 });
-
-//
-// new Vue({
-//   el: '#giffy',
-//   data: {
-//     query: "batman",
-//     header: {
-//       first: "Sprint",
-//       second: "Planning" },
-//     results: [],
-//     giffyUrl: "http://media4.giphy.com/media/7p3e2WCM0VEnm/giphy.gif"
-//     },
-//   methods: {
-//       grabGiffy: function(e) {
-//         var orig = this;
-//         Vue.http.get("http://api.giphy.com/v1/gifs/search?q="+this.query+"&api_key=dc6zaTOxFJmzC&limit=16&offset=0", function(data){
-//           orig.results = [];
-//           for(index in data.data) {
-// orig.results.push(data.data[index].images.original.url);
-//           }
-//       });
-//     },
-//     selectGiffy: function(index) {
-//       this.giffyUrl = this.results[index];
-//     }
-//   }
-//
-// });
